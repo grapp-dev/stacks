904de0b1154f33cb31851e3d869816a0381f7a12
Marcin Dziewulski <hello@mobily.pl>
2020-04-05 22:32:12 +0200

    Update package script

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,7 +2,7 @@
 
 yarn clean
 yarn build
-# yarn version --no-git-tag-version --no-commit-hooks
+yarn version --no-git-tag-version --no-commit-hooks
 cp README.md dist
 cp package.json dist
 # np --yolo --no-yarn --contents=dist
