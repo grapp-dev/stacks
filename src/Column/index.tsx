ba156070cee28f2fc029fcb309adb6454a85fa9c
Marcin Dziewulski <hello@mobily.pl>
2020-04-05 22:30:44 +0200

    Add align prop (Column)

--- src/Column/index.tsx
+++ src/Column/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { View, ViewProps } from 'react-native'
-import { flexOf, Flex } from '../utils'
+import { flexOf, Flex, AxisX, alignTo } from '../utils'
 
 interface Props {
   children: React.ReactNode
   width?: Flex
+  align?: AxisX
   style?: ViewProps['style']
   testID?: ViewProps['testID']
 }
 
 export const Column = (props: Props) => {
-  const { children, width, style, testID } = props
+  const { children, width, align, style, testID } = props
 
   return (
-    <View style={[style, flexOf(width)]} testID={testID}>
+    <View style={[style, flexOf(width), alignTo(align)]} testID={testID}>
       {children}
     </View>
   )
