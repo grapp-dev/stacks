4e7f831d52bc1817ffdd5ce65e75844939d9372f
Marcin Dziewulski <hello@mobily.pl>
2020-04-11 22:44:33 +0200

    Attach remark-containers plugin

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -79,6 +79,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+          remarkPlugins: [require('remark-containers')],
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
