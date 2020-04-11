a823bf7a806eeecbb343b0da6bed672759d06f4f
Marcin Dziewulski <hello@mobily.pl>
2020-04-11 09:37:39 +0200

    Add Placeholder component (example)

---
+++ example/src/components/Placeholder/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, ViewProps } from 'react-native'
+
+import { styles } from './styles'
+
+interface Props {
+  width?: number | string
+  height?: number | string
+  style?: ViewProps['style']
+}
+
+export const Placeholder = (props: Props) => {
+  const { width = '100%', height = 60, style } = props
+
+  return <View style={[styles.root, { width, height }, style]} />
+}
---
+++ example/src/components/Placeholder/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  root: {
+    backgroundColor: '#ddd',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#999',
+  },
+})
