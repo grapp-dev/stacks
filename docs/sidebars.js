04d0faf07767264526771a90fc3bf517d86de17b
Marcin Dziewulski <hello@mobily.pl>
2020-04-12 17:44:25 +0200

    Update sidebar content (docs)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     'Getting Started': ['motivation', 'installation', 'prerequisites'],
     Components: ['stack', 'columns', 'column', 'tiles', 'inline', 'box', 'fill-object'],
-    Other: ['provider', 'grid'],
+    Other: ['provider', 'grid', 'use-spacing', 'styles'],
   },
 }
