a5864eaab2cb0983b5b459d3ec2911e6050323a3
Marcin Dziewulski <hello@mobily.pl>
2020-04-12 13:33:00 +0200

    Update Inline component (add align prop)

--- src/Inline/index.tsx
+++ src/Inline/index.tsx
@@ -1,34 +1,36 @@
 import React, { Children, cloneElement, isValidElement } from 'react'
 import { View, ViewProps } from 'react-native'
-import { lastFactory, wrapOf, directionOf, styles } from '../utils'
+import { wrapOf, directionOf, justifyTo, AxisX } from '../utils'
 import { useSpacing, useDebugStyle } from '../context'
 
 export interface Props {
   children: React.ReactNode
   space?: number
+  align?: AxisX
   style?: ViewProps['style']
   testID?: ViewProps['testID']
 }
 
 export const Inline = (props: Props) => {
-  const { children, space = 0, style, testID } = props
-  const isLast = lastFactory(children)
+  const { children, space = 0, style, align, testID } = props
   const margin = useSpacing(space)
   const debugStyle = useDebugStyle()
 
   return (
     <View style={style} testID={testID}>
-      <View style={[wrapOf('wrap'), directionOf('row'), { marginTop: -margin }]}>
-        {Children.map(children, (child, index) => {
+      <View
+        style={[
+          wrapOf('wrap'),
+          justifyTo(align),
+          directionOf('row'),
+          { marginTop: -margin, marginRight: -margin },
+        ]}
+      >
+        {Children.map(children, (child, _index) => {
           return isValidElement(child)
             ? cloneElement(child, {
                 ...child.props,
-                style: [
-                  child.props.style,
-                  debugStyle,
-                  isLast(index) ? styles.noMarginRight : { marginRight: margin },
-                  { marginTop: margin },
-                ],
+                style: [child.props.style, debugStyle, { marginTop: margin, marginRight: margin }],
               })
             : null
         })}
