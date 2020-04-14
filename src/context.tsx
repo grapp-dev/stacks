b886d0d633855b9e6d902975f7a7ee2f14056f93
Hosmel Quintana <hosmelq@gmail.com>
2020-04-13 23:29:44 -0600

    fix condition in useSpacing

--- src/context.tsx
+++ src/context.tsx
@@ -21,7 +21,7 @@ type UseSpacing = {
 export const useSpacing: UseSpacing = (space?: number): any => {
   const { spacing } = useContext(Context)
   const multiply = (n: number) => n * spacing
-  return space ? multiply(space) : multiply
+  return space !== undefined ? multiply(space) : multiply
 }
 
 export const useDebugStyle = () => {
