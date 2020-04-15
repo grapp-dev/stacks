0c956ff42e2941883641ce154fc60ed1cddde7cc
Marcin Dziewulski <hello@mobily.pl>
2020-04-15 21:36:42 +0200

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   verbose: true,
-  bail: true,
-  transform: {
-    '.(ts|tsx)': 'ts-jest',
-  },
-  testRegex: './src/.+\\.test\\.ts$',
-  moduleFileExtensions: ['ts', 'js'],
-  collectCoverage: true,
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   rootDir: __dirname,
+  testMatch: ['<rootDir>/__tests__/?(*.)(spec|test).(ts|tsx)'],
+  modulePaths: ['<rootDir>/src'],
+  moduleDirectories: ['<rootDir>/node_modules'],
+  testPathIgnorePatterns: ['node_modules', '<rootDir>/dist', '<rootDir>/example'],
+  restoreMocks: true,
+  transform: {
+    '^.+\\.[t|j]sx?$': ['babel-jest'],
+  },
 }
