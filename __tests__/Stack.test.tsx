1c3a3dde93cbf9b8f6219eaed26cca0a1de5ab2e
Marcin Dziewulski <hello@mobily.pl>
2020-04-15 22:25:56 +0200

    Update Stack tests

--- __tests__/Stack.test.tsx
+++ __tests__/Stack.test.tsx
@@ -5,7 +5,7 @@ import { Stack } from '../src'
 import { Placeholder, flattenStyle, flattenChildrenStyle } from './utils'
 
 describe('Stack', () => {
-  it('should distribute content vertically by default', () => {
+  it('should distribute content vertically and align content along the left side by default', () => {
     const { toJSON } = render(
       <Stack>
         <Placeholder />
@@ -15,17 +15,6 @@ describe('Stack', () => {
     const style = flattenStyle(toJSON())
 
     expect(style).toHaveProperty('flexDirection', 'column')
-  })
-
-  it('should align content along the left side by default', () => {
-    const { toJSON } = render(
-      <Stack>
-        <Placeholder />
-        <Placeholder />
-      </Stack>,
-    )
-    const style = flattenStyle(toJSON())
-
     expect(style).toHaveProperty('alignItems', 'flex-start')
   })
 
