9f1a3a3864708306504713b09079da92f8eb70e6
Marcin Dziewulski <hello@mobily.pl>
2020-04-17 23:15:09 +0200

    Add FillObject tests

---
+++ __tests__/FillObject.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render } from 'react-native-testing-library'
+import { FillObject } from '../src'
+
+import { Placeholder, flattenStyle } from './utils'
+
+describe('Grid', () => {
+  it('should fill the screen', () => {
+    const { toJSON } = render(
+      <FillObject>
+        <Placeholder />
+      </FillObject>,
+    )
+    const root = toJSON()
+    const style = flattenStyle(root)
+
+    expect(style).toMatchObject({
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+    })
+  })
+})
