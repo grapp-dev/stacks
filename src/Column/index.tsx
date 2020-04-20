214d20e2fd827714bafbebc886218c6a568246b9
Marcin Dziewulski <hello@mobily.pl>
2020-04-20 23:27:43 +0200

    Update Column component (add missing flexShrink)

--- src/Column/index.tsx
+++ src/Column/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, ViewProps } from 'react-native'
-import { setFlex, setAlign, Flex, AxisX } from '../utils'
+import { setFlex, setAlign, Flex, AxisX, styles } from '../utils'
 
 export interface Props extends ViewProps {
   children: React.ReactNode
@@ -12,7 +12,7 @@ export const Column = (props: Props) => {
   const { children, width, align, style, ...rest } = props
 
   return (
-    <View style={[style, setFlex(width), setAlign(align)]} {...rest}>
+    <View style={[style, styles.shrink, setFlex(width), setAlign(align)]} {...rest}>
       {children}
     </View>
   )
