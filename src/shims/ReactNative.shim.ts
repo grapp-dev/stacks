fe471dff0c5500baf2cb873c236f2348f647ee3a
Marcin Dziewulski <hello@mobily.pl>
2021-04-01 22:01:18 +0200

    Update RN shims

--- src/shims/ReactNative.shim.ts
+++ src/shims/ReactNative.shim.ts
@@ -1,7 +1,9 @@
 import {
   AccessibilityState,
-  AccessibilityTrait,
+  AccessibilityActionInfo,
   AccessibilityValue,
+  AccessibilityRole,
+  AccessibilityPropsAndroid,
   GestureResponderEvent,
   Insets,
   LayoutChangeEvent,
@@ -13,11 +15,13 @@ import {
 export type Event_layoutEvent = LayoutChangeEvent
 export type Event_pressEvent = GestureResponderEvent
 export type Style_t = StyleProp<ViewStyle>
-export type View_edgeInsets = Insets
 export type Accessibility_value = AccessibilityValue
 export type Accessibility_state = AccessibilityState
-export type AccessibilityTrait_t = AccessibilityTrait
+export type Accessibility_liveRegion = AccessibilityPropsAndroid['accessibilityLiveRegion']
+export type Accessibility_role = AccessibilityRole
+export type View_View_edgeInsets = Insets
 export type Dimensions_displayMetrics = ScaledSize
+export type Accessibility_actionInfo = AccessibilityActionInfo
 export type Style_size = number | string
 export type Dimensions_handler = ({
   window,
