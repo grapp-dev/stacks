832a516ace100219079e0de4ae11603994d3f839
Marcin Dziewulski <hello@mobily.pl>
2021-05-09 14:08:29 +0200

    Update esmImports plugin

--- plugins/esm-imports.js
+++ plugins/esm-imports.js
@@ -1,21 +1,18 @@
 exports.esmImports = (j, source) => {
   const root = j(source)
-  const transform = (name, module) => {
+  const transform = (fn, name, module) => {
     return root
       .find(j.ImportDeclaration)
       .filter(p => {
         return p.value.source.value === name
       })
       .replaceWith(p => {
-        return j.importDeclaration(
-          [j.importDefaultSpecifier(j.identifier(module))],
-          j.literal(name),
-        )
+        return j.importDeclaration([fn(j.identifier(module))], j.literal(name))
       })
   }
 
-  transform('react', 'React')
-  transform('react-native', 'ReactNative')
+  transform(j.importDefaultSpecifier, 'react', 'React')
+  transform(j.importNamespaceSpecifier, 'react-native', 'ReactNative')
 
   return root.toSource()
 }
