34958be05bb9136dbd1e00932c0d38b555411dc7
Marcin Dziewulski <hello@mobily.pl>
2022-01-01 21:24:23 +0100

    Update esbuild config

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,11 +1,5 @@
 const esbuild = require('esbuild')
 
-const { jscodeshift } = require('./plugins/esbuild-jscodeshift')
-const { replaceLiterals } = require('./plugins/replace-literals')
-const { uncurryFunctions } = require('./plugins/uncurry-functions')
-const { rewriteProps } = require('./plugins/rewrite-props')
-const { esmImports } = require('./plugins/esm-imports')
-
 const handleError = () => process.exit(1)
 const build = (outfile, options = {}) => {
   const { format = 'cjs' } = options
@@ -16,20 +10,8 @@ const build = (outfile, options = {}) => {
       bundle: true,
       format,
       outdir: `dist/${format}`,
-      plugins: [
-        jscodeshift({
-          exclude: ['node_modules/**'],
-          plugins: [
-            replaceLiterals,
-            uncurryFunctions,
-            rewriteProps,
-            // options.format === 'esm' ? esmImports : undefined,
-          ].filter(Boolean),
-        }),
-      ],
       treeShaking: true,
       minify: false,
-      external: [],
       logLevel: 'info',
       legalComments: 'none',
       external: ['react', 'react-native'],
