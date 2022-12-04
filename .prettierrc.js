53db9e864d3796c9d8670679bd99747d5bb42db7
Marcin Dziewulski <hello@mobily.pl>
2022-12-04 23:37:58 +0100

    chore: update config files

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: 'typescript',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-import-sort'],
+}
