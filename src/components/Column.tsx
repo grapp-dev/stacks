dce415102cf94187fd0f49603667a43ea9c6665c
Marcin Dziewulski <hello@mobily.pl>
2024-04-01 20:30:06 +0200

    feat: add Column.from, add `collapseBelow` prop

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -3,13 +3,20 @@ import * as React from 'react';
 import { BoxProps } from './Box';
 
 export const Column = (_props: BoxProps): JSX.Element => {
-  throw new Error('Column must be a direct child of Columns.');
+  throw new Error('[Stacks]: Column must be a direct child of Columns.');
 };
 
-export const markAsColumn = (node: React.FC) => {
+const markAsColumn = (node: React.FC) => {
   // @ts-expect-error: this_is_fine.png
   // eslint-disable-next-line functional/immutable-data
   node.__isColumn__ = true;
 };
 
+const from = <T extends React.ComponentProps<typeof Column>>(Component: React.FC<T>) => {
+  // @ts-expect-error: this_is_fine.png
+  Component.__isColumnForwarded__ = true;
+  return Component;
+};
+
+Column.from = from;
 markAsColumn(Column);
--- src/components/Columns.tsx
+++ src/components/Columns.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-import { AxisX, AxisY, Flex, ResponsiveProp, Space } from '../types';
-import { flattenChildren } from '../utils';
+import { AxisX, AxisY, Breakpoint, Flex, ResponsiveProp, Space } from '../types';
+import { flattenChildren, isBreakpointBelow } from '../utils';
 import { Box } from './Box';
 import { Column } from './Column';
 
@@ -10,13 +10,14 @@ type BoxProps = Omit<
   'alignX' | 'alignY' | 'direction' | 'gap' | 'rowGap' | 'columnGap'
 >;
 
-type ColumnProps = React.ComponentProps<typeof Column>;
+export type ColumnProps = React.ComponentProps<typeof Column>;
 
 export type ColumnsProps = BoxProps & {
   readonly space?: ResponsiveProp<number>;
   readonly defaultFlex?: ResponsiveProp<Flex>;
   readonly alignX?: ResponsiveProp<AxisX | Space>;
   readonly alignY?: ResponsiveProp<AxisY>;
+  readonly collapseBelow?: Breakpoint;
 };
 
 const getColumnProps = (node: React.ReactNode): ColumnProps | null => {
@@ -30,29 +31,48 @@ const getColumnProps = (node: React.ReactNode): ColumnProps | null => {
     : null;
 };
 
+const isColumnForwarded = (node: React.ReactNode): node is React.ReactElement => {
+  return (
+    node !== undefined &&
+    node !== null &&
+    typeof node === 'object' &&
+    'type' in node &&
+    // @ts-expect-error: this_is_fine.png
+    node.type.__isColumnForwarded__ &&
+    React.isValidElement(node)
+  );
+};
+
 export const Columns = (props: ColumnsProps) => {
-  const { children, space, defaultFlex = 'fluid', alignX, alignY, ...rest } = props;
+  const { children, space, defaultFlex = 'fluid', alignX, collapseBelow, ...rest } = props;
+
+  const isCollapsed = isBreakpointBelow(collapseBelow);
+  const direction = isCollapsed ? 'column' : 'row';
 
   return (
-    <Box {...rest} direction="row" gap={space} alignX={alignX}>
+    <Box {...rest} direction={direction} gap={space} alignX={alignX}>
       {React.Children.map(flattenChildren(children), child => {
+        if (isColumnForwarded(child)) {
+          return React.cloneElement(
+            child,
+            { ...child.props, flex: child.props.flex ?? defaultFlex },
+            child.props.children,
+          );
+        }
+
         const props = getColumnProps(child);
 
         if (props) {
           const { children, flex, ...rest } = props;
 
           return (
-            <Box {...rest} flex={flex ?? defaultFlex} alignY={props.alignY ?? alignY}>
+            <Box {...rest} flex={flex ?? defaultFlex}>
               {children}
             </Box>
           );
         }
 
-        return (
-          <Box flex={defaultFlex} alignY={alignY}>
-            {child}
-          </Box>
-        );
+        return <Box flex={defaultFlex}>{child}</Box>;
       })}
     </Box>
   );
