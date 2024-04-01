5f61b85fc7a978b1d0342bd9e30c2b1806384dd6
Marcin Dziewulski <hello@mobily.pl>
2024-04-01 20:31:43 +0200

    fix: omit redundant props (Tiles)

--- src/components/Tiles.tsx
+++ src/components/Tiles.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 
-import { ResponsiveProp } from '../types';
+import { AxisY, ResponsiveProp, Space } from '../types';
 import { makeWithIndex, resolveResponsiveProp, splitEvery } from '../utils';
 import { Box } from './Box';
 
 type BoxProps = Omit<
   React.ComponentProps<typeof Box>,
-  'gap' | 'rowGap' | 'columnGap' | 'direction'
+  'gap' | 'rowGap' | 'columnGap' | 'direction' | 'alignX' | 'alignY'
 >;
 
 type Props = BoxProps & {
@@ -15,24 +15,35 @@ type Props = BoxProps & {
   readonly spaceX?: ResponsiveProp<number>;
   readonly spaceY?: ResponsiveProp<number>;
   readonly empty?: ResponsiveProp<boolean>;
+  readonly alignY?: ResponsiveProp<AxisY | Space>;
 };
 
 export const Tiles = (props: Props) => {
-  const { children, columns, space, spaceX, spaceY, empty, ...rest } = props;
+  const {
+    children,
+    flex = 'content',
+    columns,
+    space,
+    spaceX,
+    spaceY,
+    empty = true,
+    reverse,
+    ...rest
+  } = props;
 
   const numberOfColumns = Math.max(resolveResponsiveProp(columns) ?? 1, 1);
-  const isEmpty = resolveResponsiveProp(empty) ?? true;
+  const isEmpty = resolveResponsiveProp(empty);
   const rows = splitEvery(React.Children.toArray(children), numberOfColumns);
 
   return (
-    <Box {...rest} direction="column" gap={space} rowGap={spaceY}>
+    <Box {...rest} flex={flex} direction="column" gap={space} rowGap={spaceY}>
       {rows.map((columns, rowIndex) => {
         const elements = makeWithIndex(numberOfColumns, index => {
           return columns[index] ?? null;
         });
 
         return (
-          <Box key={rowIndex} direction="row" gap={space} columnGap={spaceX}>
+          <Box key={rowIndex} direction="row" gap={space} columnGap={spaceX} reverse={reverse}>
             {elements.map((element, columnIndex) => {
               return React.isValidElement(element) || isEmpty ? (
                 <Box key={`${rowIndex}-${columnIndex}`} flex="fluid">
