736ef783ae14219c29826440da23e62fb4175286
Marcin Dziewulski <hello@mobily.pl>
2024-04-07 18:23:09 +0200

    chore: add tsup

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  splitting: false,
+  sourcemap: false,
+  outDir: './lib',
+  format: ['esm', 'cjs'],
+  clean: true,
+  dts: true,
+  external: ['react', 'react-native', 'react-native-unistyles'],
+  outExtension: () => {
+    return {
+      js: '.js',
+    };
+  },
+});
